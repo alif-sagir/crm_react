a8d3a493891a6343acb8e31441e844682ceb4525
Yamin Hossain <streatdot616@gmail.com>
2024-03-06 18:13:06 +0600

    skdfkjsd

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,54 @@
+import axios from 'axios';
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+    const navigate = useNavigate();
+    const loginSubmit = async (event) => {
+
+        event.preventDefault();
+        // let name = event.target.email.value;
+        // let form = {
+        //     email:event.target.email.value,
+        //     password:event.target.email.value,
+        // }
+    
+        // console.log('name value', form);
+        axios.post("api/login-submit", new FormData(event.currentTarget))
+            .then(res => {
+                // {{-- localStorage.token = res.data.access_token; --}}
+                console.log('from form submitddd',res.data?.data?.role);
+
+                // return;
+
+                if (res?.data?.data?.role === 'employee') {
+                    console.log('role', 1);
+                    // window.toaster( "success");
+                    // return location.href = "#/dashboard";
+                    return navigate("/dashboard");
+
+                }
+                // if (res?.data?.title == 'RajiburRahman') {
+                //     window.toaster( "success");
+                //     return location.href = "/dashboard";
+
+                // }
+                else{
+                    // return location.href = "/home";
+                }
+
+                console.log(res);
+                console.log(res.data.access_token);
+            })
+            .catch(err => {
+                // let { code, data } = err.response.data;
+                // window.toaster(code, "error");
+                // console.log(data);
+                // console.log(err);
+                // var parentDiv = document.getElementById('email_input');
+                // parentDiv.insertAdjacentHTML('afterend', `<Div class="form_error text-danger">${err?.response?.data?.code}</Div>`)
+            })
+    }
     return (
         <>
             {/* <h1 className='testt'>login</h1> */}
@@ -18,13 +66,14 @@ function Login() {
                             </div>
 
                             {/* form_area_start */}
-                            <form action="#">
+                            <form onSubmit={(event) =>loginSubmit(event)} method="POST" action="/login-submit" >
                                 <div class="input_area">
                                     {/* email area start */}
                                     <div class="icon_and_input_text_area">
                                        
                                         <div class="input_text_area">
-                                            <input type="email" className="input_text" placeholder="Email . . ." />
+                                            <input name='email' type="email"
+                                            value={'mmm@gmail.com'} className="input_text" placeholder="Email . . ." />
                                         </div>
                                     </div>
                                     {/* email area end */}
@@ -33,7 +82,8 @@ function Login() {
                                     <div class="icon_and_input_text_area">
                                        
                                         <div class="input_text_area">
-                                            <input type="password" class="input_text" placeholder="Enter your password . . ." />
+                                            <input name='password' type="password"
+                                             value={'1234'}  class="input_text" placeholder="Enter your password . . ." />
                                         </div>
                                     </div>
                                     {/* password area end */}
--- src/Pages/routes/partials/auth.routes.js
+++ src/Pages/routes/partials/auth.routes.js
@@ -11,6 +11,8 @@ router
 	.post("/login-submit", async function (req, res) {
 		console.log(req.body);
 		
+
+		return
 		const { email, password } = req.body;
 		console.log('pre pass', password);
 		let user = await userModel.where({ email: email }).findOne();
@@ -57,38 +59,7 @@ router
 			}
 			return res.status(401).json({ code: 'password does not match', message: 'your crediential does not match' });
 		}
-		/* if (user.username == user) {
-			console.log(user);
-			let passMatch = await bcrypt.compare(password, user.password);
-			// console.log(passMatch);
-			if (passMatch) {
-				console.log(passMatch);
-				let data = {
-					username: user.username,
-					email: user.email, 
-					_id: user._id,
-					photo_url: '',
-					device_id: '',
-					genrate_time: '', 
-				};
-				// console.log('data', req?.session?.user);
-				req.session.isAuth = true;
-				req.session.user = data;
-				var token = await jwt.sign( data , '91eb159c-a766-48c3-b143-849170dbceb8');
-				res.cookie('token',token)
-				// console.log('prev_auth_url',req.session.prev_auth_url);
-				let prevUrl = req.session.prev_auth_url;
-				if (prevUrl) {
-					delete req.session.prev_auth_url;
-					if(prevUrl != "/favicon.ico"){
-						return res.redirect(prevUrl);
-					}
-				}
-				return res.status(201).json({code: 'password match', message: 'your password match'});
-			}
-			return res.status(401).json({code: 'password does not match', message: 'your crediential does not match'});
-		} */
-
+		
 		// return res.redirect("/login");
 		return res.status(401).json({ code: 'user not found', message: 'your crediential does not match' });;
 	})
