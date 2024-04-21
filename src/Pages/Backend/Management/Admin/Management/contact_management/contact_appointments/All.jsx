8b099856f570439297bf9b181df993277d732cce
Yamin Hossain <streatdot616@gmail.com>
2024-04-21 12:04:25 +0600

    lsdjfl

--- src/Pages/Backend/Management/Admin/Management/contact_management/contact_appointments/All.jsx
+++ src/Pages/Backend/Management/Admin/Management/contact_management/contact_appointments/All.jsx
@@ -67,12 +67,12 @@ function All() {
                                         <td><input type="checkbox" className="form-check-input" /></td>
                                         <td>
                                             <span >
-                                            {item.contact_number_id}
+                                            {item?.crm_contact_number?.details}
                                             </span>
                                         </td>
                                         <td>
                                             <span>
-                                            {item.customer_id}
+                                            {item?.customer?.full_name}
                                             </span>
                                         </td>
 
